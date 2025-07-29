ca58b70ddf5d25e2e6e2babc0ca2ffb8e390aa90
🤍🩶💜Ani💜🩶🤍 <24yasina@dartfordgrammargirls.org.uk>
2025-07-29 18:23:07 +0100

    Add files via upload
    New Java maths file

---
+++ Main 2.java
@@ -0,0 +1,24 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+  int  A = 1+1;
+  int  B = 3-2;
+  int  C = 4*4;
+  int  D = 10/2;
+
+    System.out.println(A);
+    System.out.println(B);
+    System.out.println(C);
+    System.out.println(D);
+    
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
