9a4f1cb731085a6b53df1748645da370b39f3031
🤍🩶💜Ani💜🩶🤍 <24yasina@dartfordgrammargirls.org.uk>
2025-07-29 19:59:51 +0100

    Add files via upload

---
+++ Main 3.java
@@ -0,0 +1,20 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+//The String[] type specifies that the variable type is an array of strings - its like assigning a bunch of variables to 1 variable, each one differentiable by its index (number in array).//
+
+public class Main {
+  public static void main(String[] args) {
+    String[]cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    System.out.println(cars[0]);
+    System.out.println(cars[1]);
+    System.out.println(cars[2]);
+    System.out.println(cars[3]);
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
